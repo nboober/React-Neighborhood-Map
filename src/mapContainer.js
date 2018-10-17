77c164f6747213a8631933c12e2a33f2fc0ece61
Nick Boober <nboober@terpmail.umd.edu>
2018-10-17 17:46:46 -0400

    feat: Added Marker functionality in my app. Applied my first marker to my selected neighborhood

--- src/mapContainer.js
+++ src/mapContainer.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 //I found the react-google-maps module from https://medium.com/@yelstin.fernandes/render-a-map-component-using-react-google-maps-5f7fb3e418bb
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 export class MapContainer extends Component{
 
   render() {
-    const GoogleMapExample = withGoogleMap(props => (
+    const MyGoogleMap = withGoogleMap(props => (
      <GoogleMap
-       defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
+       defaultCenter = { { lat: 39.1883841, lng: -77.2407077 } }
        defaultZoom = { 13 }
      >
+     {props.isMarkerShown && <Marker position={{ lat: 39.1883841, lng: -77.2407077 }} />}
      </GoogleMap>
     ));
 
     return (
       <div>
-      <GoogleMapExample
+      <MyGoogleMap
+        isMarkerShown
+
         containerElement={
-          <div style={{ height: `100vh`, width: '100vw' }} />
+          <div style={{ height: `100%`, width: '100%'}} />
         }
+
         mapElement={
-          <div style={{ height: `93%`, width: '99%' }} />
+          <div style={{ height: `95vh`, width: '99%' }} />
         }
       />
       </div>
